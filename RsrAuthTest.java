e1cb422c356576a5e43fcf68714ce2e12ea98c8c
Matthias <39822778+matthias-at-work@users.noreply.github.com>
2019-06-06 17:38:26 +0200

    Update RsrAuthTest.java

--- RsrAuthTest.java
+++ RsrAuthTest.java
@@ -21,46 +21,46 @@ class RsrAuthTest {
 
         CLibrary INSTANCE = (CLibrary) Native.load("libfsrverify.so.1.1.4", CLibrary.class); 
 	
-		// DECLSPEC FSRTOKEN_STATUS __stdcall fsrverify_InitLoadFromFile(
-		//   LPCWSTR keystoreFile);
-		int fsrverify_InitLoadFromFile(
-			com.sun.jna.WString keystoreFile);
+	// DECLSPEC FSRTOKEN_STATUS __stdcall fsrverify_InitLoadFromFile(
+	//   LPCWSTR keystoreFile);
+	int fsrverify_InitLoadFromFile(
+		com.sun.jna.WString keystoreFile);
 
-		// The fsrverify_VerifyToken verifies if a given string is a valid token for a given username. If the
-		// given string has the format of the token, it also returns the information contained in the token. The
-		// function returns 0 if the given token is valid and the key used to verify the token's signature has not
-		// been revoked. If the token is valid but revocation checking is disabled, 17 is returned.
-		//
-		// DECLSPEC FSRTOKEN_STATUS __stdcall fsrverify_VerifyToken(
+	// The fsrverify_VerifyToken verifies if a given string is a valid token for a given username. If the
+	// given string has the format of the token, it also returns the information contained in the token. The
+	// function returns 0 if the given token is valid and the key used to verify the token's signature has not
+	// been revoked. If the token is valid but revocation checking is disabled, 17 is returned.
+	//
+	// DECLSPEC FSRTOKEN_STATUS __stdcall fsrverify_VerifyToken(
         //   LPCWSTR username,
         //   LPCWSTR tokenString,
-		//   int* keyIndex, 
-		//   int* expiryDate);
-		int fsrverify_VerifyToken(
-			com.sun.jna.WString username, 
-			com.sun.jna.WString tokenString, 
-			com.sun.jna.ptr.IntByReference keyIndex, 
-			com.sun.jna.ptr.IntByReference expiryDate);
-	}
+	//   int* keyIndex, 
+	//   int* expiryDate);
+	int fsrverify_VerifyToken(
+		com.sun.jna.WString username, 
+		com.sun.jna.WString tokenString, 
+		com.sun.jna.ptr.IntByReference keyIndex, 
+		com.sun.jna.ptr.IntByReference expiryDate);
+    }
 
     public static void main(String[] args) {
 		
-		// instantiate library from key-file
-		com.sun.jna.WString filePath = new com.sun.jna.WString("//home//mats//myjava//fsr-authentication.public.keys");
+	// instantiate library from key-file
+	com.sun.jna.WString filePath = new com.sun.jna.WString("//home//mats//myjava//fsr-authentication.public.keys");
         CLibrary.INSTANCE.fsrverify_InitLoadFromFile(filePath);
 		
-		// call verification method
-		com.sun.jna.WString username = new com.sun.jna.WString("TestFSR1");
-		com.sun.jna.WString tokenString = new com.sun.jna.WString("BTLHW-XYH2Y-WONET-6URQ");
-		com.sun.jna.ptr.IntByReference keyIndex = new com.sun.jna.ptr.IntByReference();
-		com.sun.jna.ptr.IntByReference expiryDate = new com.sun.jna.ptr.IntByReference();
+	// call verification method
+	com.sun.jna.WString username = new com.sun.jna.WString("TestFSR1");
+	com.sun.jna.WString tokenString = new com.sun.jna.WString("BTLHW-XYH2Y-WONET-6URQ");
+	com.sun.jna.ptr.IntByReference keyIndex = new com.sun.jna.ptr.IntByReference();
+	com.sun.jna.ptr.IntByReference expiryDate = new com.sun.jna.ptr.IntByReference();
         int tokenStatus = CLibrary.INSTANCE.fsrverify_VerifyToken(username, tokenString, keyIndex, expiryDate);
 
-		// visualize result
-		System.out.println("Token-status: " + tokenStatus);
-		System.out.println("Key-index: " + keyIndex.getValue());
-		// Docu: expiration date => number of days since 1st January 2011
-		System.out.println("Expiry-date (number of days since 1st January 2011): " + expiryDate.getValue());
-		System.out.println("program terminated successfully...");
+	// visualize result
+	System.out.println("Token-status: " + tokenStatus);
+	System.out.println("Key-index: " + keyIndex.getValue());
+	// Docu: expiration date => number of days since 1st January 2011
+	System.out.println("Expiry-date (number of days since 1st January 2011): " + expiryDate.getValue());
+	System.out.println("program terminated successfully...");
     }
 }
